3fd2a663fa4002579827e2eb8b3e0f0edbfa755a
Lukas Weber <me@sakul6499.de>
2022-09-05 01:01:16 +0200

    Further work on splitting

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             var client = new SaintCoinachWrapper.Client();
+            client.GetTerritoryTypesByAnyName("The World Unsundered").ToList().ForEach(territory_type => Console.WriteLine(client.TerritoryToFullString(client.LoadTerritoryFromTerritoryType(territory_type))));
         }
     }
 }
--- src/SaintCoinachWrapper/Client.cs
+++ src/SaintCoinachWrapper/Client.cs
@@ -1,3 +1,6 @@
+using SaintCoinach.Graphics;
+using SaintCoinach.Xiv;
+
 namespace SaintCoinachWrapper
 {
     public class Client
@@ -31,6 +34,101 @@ namespace SaintCoinachWrapper
         public void PrintVersionInformation()
         {
             Console.WriteLine("Definitions Version: " + this.a_realm_reversed.DefinitionVersion);
+        }
+
+        public IXivSheet<T> GetData<T>() where T : IXivRow
+        {
+            return this.a_realm_reversed.GameData.GetSheet<T>();
+        }
+
+        public T[] GetDataArray<T>() where T : IXivRow
+        {
+            return this.GetData<T>().ToArray();
+        }
+
+        public IXivSheet<TerritoryType> GetTerritoryTypes()
+        {
+            return this.GetData<TerritoryType>();
+        }
+
+        public IEnumerable<TerritoryType> GetTerritoryTypesByName(String name, Boolean strict = false)
+        {
+            if (strict)
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.Name.ToString().Contains(name));
+            }
+            else
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.Name.ToString().Equals(name));
+            }
+        }
+
+        public IEnumerable<TerritoryType> GetTerritoryTypesByPlaceName(String place_name, Boolean strict = false)
+        {
+            if (strict)
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.PlaceName.ToString().Contains(place_name));
+            }
+            else
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.PlaceName.ToString().Equals(place_name));
+            }
+        }
+
+        public IEnumerable<TerritoryType> GetTerritoryTypesByZonePlaceName(String zone_place_name, Boolean strict = false)
+        {
+            if (strict)
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.ZonePlaceName.ToString().Contains(zone_place_name));
+            }
+            else
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.ZonePlaceName.ToString().Equals(zone_place_name));
+            }
+        }
+
+        public IEnumerable<TerritoryType> GetTerritoryTypesByRegionPlaceName(String region_place_name, Boolean strict = false)
+        {
+            if (strict)
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.RegionPlaceName.ToString().Contains(region_place_name));
+            }
+            else
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.RegionPlaceName.ToString().Equals(region_place_name));
+            }
+        }
+
+        public IEnumerable<TerritoryType> GetTerritoryTypesByAnyName(String name, Boolean strict = false)
+        {
+            if (strict)
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.Name.ToString().Equals(name) || territory_type.PlaceName.ToString().Equals(name) || territory_type.ZonePlaceName.ToString().Equals(name) || territory_type.RegionPlaceName.ToString().Equals(name));
+            }
+            else
+            {
+                return this.GetData<TerritoryType>().Where(territory_type => territory_type.Name.ToString().Contains(name) || territory_type.PlaceName.ToString().Contains(name) || territory_type.ZonePlaceName.ToString().Contains(name) || territory_type.RegionPlaceName.ToString().Contains(name));
+            }
+        }
+
+        public Territory LoadTerritoryFromTerritoryType(TerritoryType territoryType)
+        {
+            return new Territory(territoryType);
+        }
+
+        public IEnumerable<Territory> ConvertTerritoryTypesToTerritories(IEnumerable<TerritoryType> territory_types)
+        {
+            return territory_types.Select(territory_type => this.LoadTerritoryFromTerritoryType(territory_type));
+        }
+
+        public String TerritoryToFullString(Territory territory)
+        {
+            return this.TerritoryTypeToString(this.GetTerritoryTypesByName(territory.Name, true).First());
+        }
+
+        public String TerritoryTypeToString(TerritoryType territory_type)
+        {
+            return $"{territory_type.RegionPlaceName}/{territory_type.ZonePlaceName}/{territory_type.PlaceName}/{territory_type.Name}";
         }
     }
 }
